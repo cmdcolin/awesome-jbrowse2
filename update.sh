1dc39d6cde9bfbd6d09d48b61dd3390065545d62
Colin <colin.diesh@gmail.com>
2023-01-07 11:32:56 -0700

    Add treenome browser

--- update.sh
+++ update.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd img;
-identify -format "%f %wx%h\n" *.jpg *.png > ../dims.txt
+identify -format "%f %wx%h\n" *.jpg *.jpeg *.png > ../dims.txt
 cd -
 node get_dimensions.js
 node get_citations.js
