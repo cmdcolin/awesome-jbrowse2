4a0b6884ce8ea649828f8fb95fed5a7c7a2669d3
Colin <colin.diesh@gmail.com>
2025-03-02 20:21:49 -0500

    Tags

--- gen_readme.js
+++ gen_readme.js
@@ -1,23 +1,25 @@
-import fs from 'fs'
-const data = JSON.parse(fs.readFileSync('LINKS.json', 'utf8'))
-console.log('# awesome-jbrowse2')
+import fs from "fs";
+const data = JSON.parse(fs.readFileSync("LINKS.json", "utf8"));
+console.log("# awesome-jbrowse2");
 
 console.log(
-  'awesome jbrowse 2 citations, screenshots or instances around the web. visit website here https://cmdcolin.github.io/awesome-jbrowse2\n\n## Publications/instances',
-)
+  "awesome jbrowse 2 citations, screenshots or instances around the web. visit website here https://cmdcolin.github.io/awesome-jbrowse2\n\n## Publications/instances",
+);
 
-console.log('name|instance/url|note|img')
-console.log('-----|----|----|----')
-data.links.forEach(link => {
+console.log("tags|name|instance/url|note|img");
+console.log("----|-----|----|----|----");
+data.links.forEach((link) => {
   console.log(
-    link.name || '',
-    '|',
-    link.pub?.title || '',
-    link.pub?.doi ? `([pub](${link.pub.doi}))` : '',
-    link.url ? `([url](${link.url}))` : '',
-    '|',
-    link.note || '',
-    '|',
-    link.img ? `[img](img/${link.img})` : '',
-  )
-})
+    link.tags?.join(",") || "",
+    "|",
+    link.name || "",
+    "|",
+    link.pub?.title || "",
+    link.pub?.doi ? `([pub](${link.pub.doi}))` : "",
+    link.url ? `([url](${link.url}))` : "",
+    "|",
+    link.note || "",
+    "|",
+    link.img ? `[img](img/${link.img})` : "",
+  );
+});
